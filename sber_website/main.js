b2e71400f4b76070c5186d448923dc83eed0898b
Vitaliya Gubanova <vitaliagubanova@gmail.com>
2025-04-17 01:46:27 +0300

    4

--- sber_website/main.js
+++ sber_website/main.js
@@ -1,3 +1,7 @@
+import {MainPage} from "./pages/main/index.js"; 
 const root = document.getElementById('root');
 
-root.insertAdjacentHTML('beforeend', '<button type="button" class="btn btn-primary">Hello world 2!</button>')
+const mainPage = new MainPage(root);
+
+mainPage.render();
+
---
+++ sber_website/pages/main/index.js
@@ -0,0 +1,13 @@
+import {ProductCardComponent} from "../../components/product-card/index.js";
+export class MainPage {
+    constructor(parent) {
+        this.parent = parent;
+    }
+    
+    render() {
+        const productCard = new ProductCardComponent(this.parent)
+        productCard.render()
+    }
+    
+}
+
